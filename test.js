e316dae22de2865a3da97defde22b67aa35d23c2
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-23 08:58:51 +0100

    setup base
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,6 +1,49 @@
 
+const setupBase = (require('setuprc')).setupBase;
 const nanoTest  = new (require('nanoTest')).test({
     'debugPrint' : 'short'
 });
-const screen = new (require('./index.js')).screenBase();
+const setupTest = new setupBase({
+    'debugPrint':{
+        'type'    : 'select',
+        'list'    : [
+            'normal',
+            'short'
+        ],
+        'default' : 'normal'
+    },
+    'exitCodeFail':{
+        'type'    : 'select',
+        'list'    : [
+            '1',
+            '0',
+        ],
+        'default' : '1'
+    },
+    'exitCodeError':{
+        'type'    : 'select',
+        'list'    : [
+            '1',
+            '0',
+        ],
+        'default' : '1'
+    },
+    'exitCodeMissing':{
+        'type'    : 'select',
+        'list'    : [
+            '1',
+            '0',
+        ],
+        'default' : '1'
+    },
+    'progressBar':{
+        'type'    : 'bool',
+        'default' : true
+    }
+});
+
+setupTest.setup({
+    'progressBar':false
+})
+const screen = new (require('./index.js')).screenBase({},setupTest);
 nanoTest.run();
