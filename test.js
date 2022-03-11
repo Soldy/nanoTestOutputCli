099f58b2d0551e5370c011a6974cb148c2ec963a
Soldy <4786022+Soldy@users.noreply.github.com>
2022-03-11 10:21:53 +0000

    test with new rules
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,49 +1,122 @@
 
 const setupBase = (require('setuprc')).base;
-const nanoTest  = new (require('nanoTest')).test({
-    'debugPrint' : 'short'
-});
+const $result = {
+   all: 4,
+   ok: 1,
+   fail: 1,
+   error: 1,
+   missing: 1
+};
+const $test_ok ={
+            'name'     : 'test ok',
+            'test'     : ()=>{},
+            'rule'     : "===",
+            'sample'   : 0,
+            'ready'    : false,
+            'startTime': 11111124,
+            'endTime'  : 11111125,
+            'time'     : 1,
+            'result'   : 1,
+            'error'    : false,
+            'value'    : '',
+            'check'    : false,
+            'debug'    : '',
+            'willfail' : false
+};
+const $test_failed ={
+            'name'     : 'test ok',
+            'test'     : ()=>{},
+            'rule'     : "===",
+            'sample'   : 0,
+            'ready'    : false,
+            'startTime': 11111124,
+            'endTime'  : 11111125,
+            'time'     : 1,
+            'result'   : 2,
+            'error'    : false,
+            'value'    : '',
+            'check'    : false,
+            'debug'    : '',
+            'willfail' : false
+};
+const $test_missing ={
+            'name'     : 'test ok',
+            'test'     : ()=>{},
+            'rule'     : "===",
+            'sample'   : 0,
+            'ready'    : false,
+            'startTime': 11111124,
+            'endTime'  : 11111125,
+            'time'     : 1,
+            'result'   : 3,
+            'error'    : false,
+            'value'    : '',
+            'check'    : false,
+            'debug'    : '',
+            'willfail' : false
+};
+const $test_error ={
+            'name'     : 'test ok',
+            'test'     : ()=>{},
+            'rule'     : "===",
+            'sample'   : 0,
+            'ready'    : false,
+            'startTime': 11111124,
+            'endTime'  : 11111125,
+            'time'     : 1,
+            'result'   : 4,
+            'error'    : false,
+            'value'    : '',
+            'check'    : false,
+            'debug'    : '',
+            'willfail' : false
+};
+
 const setupTest = new setupBase({
-    'debugPrint':{
-        'type'    : 'select',
-        'list'    : [
-            'normal',
-            'short'
-        ],
-        'default' : 'normal'
-    },
-    'exitCodeFail':{
-        'type'    : 'select',
-        'list'    : [
-            '1',
-            '0',
-        ],
-        'default' : '1'
-    },
-    'exitCodeError':{
-        'type'    : 'select',
-        'list'    : [
-            '1',
-            '0',
-        ],
-        'default' : '1'
-    },
-    'exitCodeMissing':{
-        'type'    : 'select',
-        'list'    : [
-            '1',
-            '0',
-        ],
-        'default' : '1'
-    },
-    'progressBar':{
-        'type'    : 'bool',
-        'default' : true
-    }
+        'debug_print':{
+            'type'    : 'select',
+            'list'    : [
+                'normal',
+                'short'
+            ],
+            'default' : 'normal'
+        },
+        'progress_bar':{
+            'type'    : 'bool',
+            'default' : true
+        },
+        'exit_code_fail':{
+            'type'    : 'bool',
+            'default' : true
+        },
+        'exit_code_error':{
+            'type'    : 'bool',
+            'default' : true
+        },
+        'exit_code_missing':{
+            'type'    : 'bool',
+            'default' : true
+        }
 });
 
 setupTest.setup({
     'progressBar':false
 })
-const screen = new (require('./index.js')).base({},setupTest);
-nanoTest.run();
+const screen = new (require('./index.js')).base($result, setupTest);
+screen.change(
+    $result,
+    $test_ok
+);
+screen.change(
+    $result,
+    $test_failed
+);
+screen.change(
+    $result,
+    $test_missing
+);
+screen.change(
+    $result,
+    $test_error
+);
+screen.end();
